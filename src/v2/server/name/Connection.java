38cbe955353cfcb3dfa7a38fc961f7cef2c6a7ab
KingoBarnet <kingo9467@hotmail.com>
2022-09-29 14:36:10 +0200

    dsg

---
+++ src/v2/server/name/Connection.java
@@ -0,0 +1,13 @@
+package v2.server.name;
+
+public class Connection {
+
+  public final String name;
+  public final String address;
+
+  public Connection(String name, String address) {
+    this.name = name;
+    this.address = address;
+  }
+
+}
