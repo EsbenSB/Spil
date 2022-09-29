2dfad398b008d6247fba70064440a1919e08f279
KingoBarnet <kingo9467@hotmail.com>
2022-09-29 11:21:05 +0200

    zx<vcxz<

---
+++ src/Sound/test.java
@@ -0,0 +1,10 @@
+package Sound;
+
+public class test {
+
+    public static void main(String[] args) {
+        AudioPlayerExample1 test = new AudioPlayerExample1();
+
+        test.play("src/Sound/test.wav");
+    }
+}
